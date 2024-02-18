76893fe97082961de2ad28b6ac2b9c4314de6bf3
TheBenedict69 <sabenedict09@gmail.com>
2024-02-18 19:27:25 +0530

    v0.2.0 OTP page added

---
+++ lib/Pages/OTP.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class OTPpage extends StatefulWidget {
+  const OTPpage({super.key});
+
+  @override
+  State<OTPpage> createState() => _OTPpageState();
+}
+
+class _OTPpageState extends State<OTPpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xffCC2A49),
+            Color(0xffF99E4C),
+            Color(0xffF36F38),
+            Color(0xffEF4648),
+            Color(0xff582841),
+          ],
+          stops: [0.0, 0.25, 0.5, 0.75, 1.0],
+        )),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 200,
+              ),
+              Text(
+                'We have send a verification code:',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(21))),
+                    ),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 200,
+                    child: Lottie.asset('assets/Animations/PeekingCat.json'),
+                  ),
+                ],
+              ),
+              Container(
+                height: 60,
+                width: 350,
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(21)),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Verify OTP',
+                    style: Theme.of(context).textTheme.headline1?.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(21))),
+                      backgroundColor: MaterialStatePropertyAll(Colors.black)),
+                ),
+              ),
+              SizedBox(
+                height: 200,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:spareshare/Pages/EmailLogin.dart';
+import 'package:spareshare/Pages/OTP.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -27,104 +28,117 @@ class _LoginPageState extends State<LoginPage> {
               Color(0xffEF4648),
               Color(0xff582841)
             ])),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-                width: 200,
-                height: 200,
-                child: Lottie.asset('assets/Animations/Login.json')),
-            SizedBox(
-              height: 50,
-            ),
-            Container(
-              width: 350,
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Sign in:',
-                style: TextStyle(fontFamily: 'Nunito', fontSize: 30),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 125,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
+              Container(
+                  width: 200,
+                  height: 200,
+                  child: Lottie.asset('assets/Animations/Login.json')),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
                 width: 350,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(21),
-                ),
-                child: TextField(
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.phone),
-                      hintText: 'Enter Phone Number',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(21))),
-                )),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              height: 60,
-              width: 350,
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(21)),
-              child: ElevatedButton(
-                onPressed: () {},
+                alignment: Alignment.topLeft,
                 child: Text(
-                  'Send OTP',
-                  style: TextStyle(
-                      color: Colors.white, fontFamily: 'Nunito', fontSize: 24),
+                  'Sign in:',
+                  style: TextStyle(fontFamily: 'Nunito', fontSize: 30),
                 ),
-                style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(21))),
-                    backgroundColor: MaterialStatePropertyAll(Colors.black)),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              '-----------------------   OR   -----------------------',
-              style: TextStyle(fontSize: 21, color: Colors.white),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              height: 60,
-              width: 350,
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(21)),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return EmailLogin();
-                  }));
-                },
-                child: Row(
-                  children: [
-                    Icon(Icons.email),
-                    SizedBox(
-                      width: 30,
-                    ),
-                    Text(
-                      'Continue with Email',
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                  ],
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                  width: 350,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(21),
+                  ),
+                  child: TextField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.phone),
+                        hintText: 'Enter Phone Number',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(21))),
+                  )),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 60,
+                width: 350,
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(21)),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return OTPpage();
+                    }));
+                  },
+                  child: Text(
+                    'Send OTP',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Nunito',
+                        fontSize: 24),
+                  ),
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(21))),
+                      backgroundColor: MaterialStatePropertyAll(Colors.black)),
                 ),
-                style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(21))),
-                    backgroundColor: MaterialStatePropertyAll(Colors.white)),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                '-----------------------   OR   -----------------------',
+                style: TextStyle(fontSize: 21, color: Colors.white),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 60,
+                width: 350,
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(21)),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return EmailLogin();
+                    }));
+                  },
+                  child: Row(
+                    children: [
+                      Icon(Icons.email),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Text(
+                        'Continue with Email',
+                        style: Theme.of(context).textTheme.headline1,
+                      ),
+                    ],
+                  ),
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(21))),
+                      backgroundColor: MaterialStatePropertyAll(Colors.white)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
